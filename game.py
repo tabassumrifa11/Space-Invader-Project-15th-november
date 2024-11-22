a37fa4edc86d6cbd744e1381aa3c413b68abe45f
Tahsin Tabassum <tabassumrifa03@gmail.com>
2024-11-22 17:36:49 +0600

    gameee

--- game.py
+++ game.py
@@ -90,3 +90,112 @@ def isCollision(enemyX, enemyY, bulletX, bulletY):
         return True
     else:
         return False
+    
+running = True
+while running:
+    
+    screen.fill((0, 0, 0))
+    
+    screen.blit(background, (0, 0))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+            
+        if event.type == pygame.KEYDOWN:
+            
+            if event.key == pygame.K_LEFT:
+                playerX_change = -5
+                
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 5
+                
+            if event.key == pygame.K_SPACE:
+                
+                
+                if bullet_state == "ready":
+                    
+                    bulletSound = mixer.Sound("laser.wav")
+                    bulletSound.play()
+                    
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
+                    
+                    
+            if event.type == pygame.KEYUP:
+                
+                if event.key == pygame.K_LEFT or  event.key == pygame.K_RIGHT:
+                    playerX_change = 0
+                    
+                    
+                    
+    playerX = playerX + playerX_change
+    
+    if playerX <= 0:
+        playerX = 1
+        
+    elif playerX >= 736:
+        playerX = 730
+        
+        
+    for i in range(num_of_enemies):
+
+        if enemyY[i] > 340:
+
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000
+
+            game_over_text()
+            break
+        
+        
+        enemyX[i] = enemyX[i] + enemyX_change[i]
+
+
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 4
+            enemyY[i] = enemyY[i] + enemyY_change[i]
+
+
+        elif enemyX[i] >= 736:
+            enemyX_change[i] = -4
+            enemyY[i] = enemyY[i] + enemyY_change[i]
+            
+            
+        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+
+
+        if collision:
+
+            explosionSound = mixer.Sound("explosion.wav")
+            explosionSound.play()
+            bulletY = 380
+            bullet_state = "ready"
+            score_value = score_value + 1
+            enemyX[i] = random.randint(0, 736)
+            enemyY[i] = random.randint(50, 150)
+
+
+        enemy(enemyX[i], enemyY[i], i)
+        
+    if bulletY <= 0:
+        bulletY = 380
+        bullet_state = "ready"
+
+    if bullet_state == "fire":
+        fire_bullet(bulletX, bulletY)
+        bulletY = bulletY - bulletY_change
+
+    player(playerX, playerY)
+    show_score(textX, testY)
+    pygame.display.update()
+                
+                
+                    
+        
+                
+                
+            
+        
+        
